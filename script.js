f74a34cc75c1bc862764197e72f35c329f2a6743
KURKAN <ramazankurkan@gmail.com>
2023-07-03 15:08:20 +0300

    Init

---
+++ script.js
@@ -0,0 +1,44 @@
+// Get the canvas element
+var canvas = document.getElementById("paintCanvas");
+var ctx = canvas.getContext("2d");
+
+// Set initial drawing properties
+var isDrawing = false;
+var lineWidth = 5;
+var strokeColor = "#000000";
+
+// Event listeners for drawing
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stopDrawing);
+canvas.addEventListener("mouseout", stopDrawing);
+
+// Function to start drawing
+function startDrawing(event) {
+  isDrawing = true;
+  ctx.beginPath();
+  ctx.moveTo(
+    event.clientX - canvas.offsetLeft,
+    event.clientY - canvas.offsetTop
+  );
+}
+
+// Function to draw
+function draw(event) {
+  if (isDrawing) {
+    ctx.lineTo(
+      event.clientX - canvas.offsetLeft,
+      event.clientY - canvas.offsetTop
+    );
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeColor;
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.stroke();
+  }
+}
+
+// Function to stop drawing
+function stopDrawing() {
+  isDrawing = false;
+}
